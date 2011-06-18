67d154b24c06ff89579e28adcc8c5ba361b10864
Darell Tan <darell.tan@gmail.com>
2011-06-18 17:03:11 +0800

    Initial import.

---
+++ README.markdown
@@ -0,0 +1,70 @@
+nps-iphone-smsimport
+=====================
+**nps-iphone-smsimport** allows you to import SMSes from Samsung phones, 
+via the Samsung New PC Studio (NPS) software, into the iPhone SMS database.
+
+The iPhone SMS database can be accessed directly if you are using a 
+jailbroken iPhone, using iPhone Explorer or SSH/SFTP directly.
+Alternatively, you can also work on the SMS database that was backed 
+up by iTunes, then subsequently restore to the iPhone.
+
+**You should backup your data before using this program.**
+
+
+Typical Usage
+--------------
+1. Obtain iPhone SMS database, either from an iTunes backup or 
+   transferred from a jailbroken phone
+
+2. Sync your Samsung phone with the New PC Studio software
+
+3. Run the `nps-iphone-smsimport.py` script
+
+The script will then find all SMSes in the NPS database. If `--after-date` is 
+specified, only SMSes after the specified date will be processed.
+
+Duplicate SMSes will not be inserted. SMSes with the same date/time, same 
+text content and direction (sent or received) and same phone number will be 
+considered duplicates.
+
+
+Requirements
+-------------
+You need at least Python 2.6 running Windows on the same machine as the 
+Samsung NPS software, or you could copy the database and manually specify
+its path.
+
+    - Samsung NPS database
+    - Python 2.6 on Windows
+    - Python for Windows extensions (pywin32)   
+	  <http://sourceforge.net/projects/pywin32/>
+    - Jailbroken iPhone -or- iTunes backup of the iPhone
+
+
+TODO
+-----
+- Figure out the `hash` column in the `msg_group` table
+
+
+License
+--------
+It should be noted that the bundled *phonenumbers Python library* is **NOT** 
+covered under this license. For that, please refer to 
+<https://github.com/daviddrysdale/python-phonenumbers>
+
+Copyright (C) 2011 Darell Tan
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
---
+++ nps-iphone-smsimport.py
@@ -0,0 +1,358 @@
+#
+# nps-iphone-smsimport.py - Samsung NPS to iPhone SMS database importer
+# Copyright (C) 2011 Darell Tan
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+#
+
+import os, sys
+import operator
+import getopt
+
+from sqlite3 import dbapi2 as sqlite
+import win32com.client
+from   win32com.shell import shell, shellcon
+import phonenumbers
+
+
+class iPhoneSMSDB:
+	"""Class to query and manipulate the iPhone SMS Database."""
+
+	db = None
+	default_country = None
+
+	def __init__(self, default_country, sms_db):
+		if not os.path.isfile(sms_db):
+			raise IOError('database doesn\'t exist: ' + sms_db)
+
+		self.db = sqlite.connect(sms_db)
+		self.default_country = default_country.upper()
+
+		# register the user-defined function used by triggers
+		# 2nd bit is the "read" bit
+		self.db.create_function('read', 1, lambda f: (int(f) & 0x02) >> 1)
+
+
+	def __del__(self):
+		if self.db:
+			self.db.close()
+
+
+	def commit(self):
+		"""Commits the database"""
+		self.db.commit()
+
+
+	def _form_address_query(self, address):
+		numbers = [address,]
+		try:
+			pnumber = phonenumbers.parse(address, self.default_country)
+			numbers.append(phonenumbers.format_number(pnumber, 
+								phonenumbers.PhoneNumberFormat.NATIONAL))
+			numbers.append(phonenumbers.format_number(pnumber, 
+								phonenumbers.PhoneNumberFormat.INTERNATIONAL))
+		except:
+			pass
+
+		# strip spaces
+		numbers = set([x.replace(' ', '') for x in numbers])
+
+		# prepare as SQL IN() fragment
+		return "replace(address,' ','') IN (%s)" % \
+					(','.join(["'%s'" % (x) for x in numbers]))
+
+
+	def _dict_to_sql_insert(self, sql_stm, d):
+		"""Converts a dict and SQL statement prefix, returns an INSERT 
+		prepared statement and values."""
+
+		cols = []
+		values = []
+		for k, v in d.iteritems():
+			cols.append(k)
+			values.append(v)
+
+		return '%s(%s) VALUES(%s)' % (
+					sql_stm, 
+					','.join(cols), 
+					','.join(['?'] * len(values))), \
+				values
+
+
+	def get_latest_sms_date(self):
+		c = self.db.cursor()
+		c.execute("SELECT MAX(date) FROM message;")
+		latest_sms_ts = c.fetchone()[0]
+		return latest_sms_ts
+
+
+	def get_group_id(self, address):
+		"""Retrieves the group_id given an "address"."""
+
+		c = self.db.cursor()
+		c.execute("SELECT group_id FROM group_member " + 
+				"WHERE " + self._form_address_query(address))
+		res = c.fetchone()
+		return res and res[0] or None
+
+
+	def add_group(self, address):
+		"""Adds a group for the given "address" and returns the group_id."""
+
+		c = self.db.cursor()
+		c.execute("INSERT INTO msg_group(type, unread_count, hash) " + 
+				"VALUES(0, 0, NULL)")
+		group_id = c.lastrowid
+
+		country = get_number_country(address, self.default_country)
+		c.execute("INSERT INTO group_member(group_id, address, country) " + 
+				"VALUES(?, ?, ?)", 
+				(group_id, address, country))
+
+		return group_id
+
+
+	def sms_exists(self, sms):
+		"""Tests if the specified SMS (dict) already exists.
+		Matches SMS contents (text), date and "address"."""
+
+		c = self.db.cursor()
+		c.execute('SELECT * FROM message ' + 
+					'WHERE text = ? AND date = ? AND (flags & 1) = ? AND ' +
+					self._form_address_query(sms['address']), 
+						(sms['text'], sms['date'], sms['flags'] & 1))
+		return c.fetchone() is not None
+
+
+	def insert_sms(self, sms):
+		"""Inserts the given SMS.
+		Checks if the address of the SMS already has a group, otherwise calls 
+		add_group() to add one before inserting the SMS."""
+
+		address = sms['address']
+		group_id = self.get_group_id(address)
+		if group_id is None:
+			group_id = self.add_group(address)
+
+		# fill in the country of the number
+		sms['country'] = get_number_country(address, self.default_country)
+
+		# update group_id in sms
+		sms['group_id'] = group_id
+
+		# add additional columns
+		opts = {
+				'replace':			0,
+				'association_id':	0,
+				'height':			0,
+				'UIFlags':			4,
+				'version':			0,
+				}
+
+		sms = dict(sms.items() + opts.items())
+
+		stm, vals = self._dict_to_sql_insert('INSERT INTO message', sms)
+
+		c = self.db.cursor()
+		c.execute(stm, vals)
+
+
+def get_number_country(number, default_country):
+	"""Retrieves the country code for a given number. If no international 
+	prefix was found, uses the specified "default_country"."""
+
+	country = default_country
+	try:
+		p = phonenumbers.parse(number, default_country.upper())
+		country = phonenumbers.region_code_for_number(p)
+	except phonenumbers.NumberParseException:
+		pass
+	return country.lower()
+
+
+def read_NPS_sms(nps_db_path=None, filters=[]):
+	"""Reads SMSes from the Samsung New PC Studio (NPS) internal database."""
+
+	# locate NPS database
+	if not nps_db_path:
+		appdata_dir = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, None, 0)
+		nps_db_path = os.path.join(appdata_dir, 'Samsung', 'New PC Studio', 'Guest.dat')
+
+	if not os.path.isfile(nps_db_path):
+		raise IOError('unable to find Samsung NPS database at ' + nps_db_path)
+
+	adoconn = win32com.client.Dispatch(r'ADODB.Connection')
+	DSN = 'PROVIDER=Microsoft.Jet.OLEDB.4.0;DATA SOURCE=' + nps_db_path
+	adoconn.Open(DSN)
+	rs = win32com.client.Dispatch(r'ADODB.Recordset')
+
+	rs.Cursorlocation = 3;
+	filters.append("TYPE = 'SMS'")
+	rs.Open("SELECT *, IIF(SENDER IS NOT NULL, 2, 3) AS FLAGS " +
+			"FROM MESSAGE WHERE " + 
+			" AND ".join(filters), adoconn);
+
+	sms = []
+	if (rs.RecordCount):
+		while not rs.EOF:
+			address = ''
+			if rs.Fields.Item('Sender').Value:
+				address = rs.Fields.Item('Sender').Value
+			else:
+				address = rs.Fields.Item('Receiver').Value
+
+			# strip trailing semicolon
+			address = address.replace(';', '')
+
+			s = {
+				'address':	address,
+				'text':		rs.Fields.Item('Content').Value,
+				'date':		int( rs.Fields.Item('Create_date').Value ),
+				'flags':	rs.Fields.Item('Flags').Value,
+				}
+
+			sms.append(s)
+
+			rs.MoveNext()
+
+	return sms
+
+def print_usage():
+	print """
+NPS SMS Importer.
+
+%s --country <country> [--smsdb <x> | --npsdb <x> | --yes | --verbose ] ...
+
+args:
+
+  --country <country>
+      Specifies the country code, for mobile numbers without 
+      international prefix. Usually the country where your SMSes originate.
+
+  --after-date <mm/dd/yyyy>
+      Only import SMSes after the specified date. The date format must be 
+	  understood by MS Access, or just use the "mm/dd/yyyy" format.
+
+  --smsdb <iphone-sms.db>
+      Specifies iPhone SMS database file
+	  By default, "sms.db" in the current directory is used
+
+  --npsdb <nps-db.dat>
+      Specifies the Samsung NPS database file (usually called "Guest.dat")
+      By default, this is at "%%AppData%%\Samsung\New PC Studio\Guest.dat"
+
+  --yes
+      Supresses the prompt to commit the imported SMSes
+
+  --verbose
+      Prints all the SMS in the NPS database
+""" % (sys.argv[0])
+	
+
+if __name__ == '__main__':
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], '', [
+					'smsdb=', 
+					'npsdb=', 
+					'country=', 
+					'after-date=', 
+					'yes', 
+					'verbose'
+					])
+	except getopt.GetoptError, err:
+		print 'error: ', str(err)
+		print_usage()
+		sys.exit(2)
+
+	nps_db_path = None
+	sms_db_path = 'sms.db'
+	verbose = False
+	skip_prompt = False
+	country = None
+	after_date = ''
+
+	for opt, arg in opts:
+		if opt == '--smsdb':
+			sms_db_path = arg
+		elif opt == '--npsdb':
+			nps_db_path = arg
+		elif opt == '--country':
+			country = arg
+		elif opt == '--after-date':
+			after_date = arg
+		elif opt == '--yes':
+			skip_prompt = True
+		elif opt == '--verbose':
+			verbose = True
+
+	if not country:
+		print "error: country was not specified"
+		print_usage()
+		sys.exit(2)
+
+	# process NPS filters
+	nps_filters = []
+	if after_date:
+		nps_filters.append("CREATE_DATE >= #" + after_date + "#")
+
+	nps_sms = read_NPS_sms(nps_db_path, nps_filters)
+	isms = iPhoneSMSDB(country, sms_db_path)
+
+	count_total		= 0
+	count_empty		= 0
+	count_dup		= 0
+	count_inserted	= 0
+	count_newgrp	= 0
+
+	for s in sorted(nps_sms, key=operator.itemgetter('date')):
+		count_total += 1
+
+		if not s['text'] or not s['text'].strip():
+			count_empty += 1
+			print "skipping empty SMS", s
+			continue
+
+		if isms.sms_exists(s):
+			if verbose: print "duplicate SMS", s
+			count_dup += 1
+		else:
+			if verbose:
+				if not isms.get_group_id(s['address']):
+					print "adding group for", s['address']
+					count_newgrp += 1
+				print "inserting SMS", s
+			isms.insert_sms(s)
+			count_inserted += 1
+
+	print
+	print "new groups:\t", count_newgrp
+	print
+	print "empty:\t\t", count_empty
+	print "duplicate:\t", count_dup
+	print "inserted:\t", count_inserted
+	print "TOTAL:\t\t", count_total
+	print
+
+	do_commit = True
+	if not skip_prompt:
+		do_commit = raw_input("commit? (y/N) ").strip().lower() == 'y'
+
+	if do_commit:
+		print "committing...",
+		isms.commit()
+		print "done"
+	else:
+		print "not commited"
+
